bf57f399194beda7f6512723e0e8f9245fd486cb
edvin-W <wendelhagedvin@gmail.com>
2025-10-27 14:35:48 +0100

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -8,6 +8,12 @@ Date: [Date]
 import socket
 import sys
 
+def start_server(host, port):
+    pass
 
 if __name__ == "__main__":
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    print(f"The hostname is: {hostname}")
+    print(f"THe IP is: {ip}")
     pass
